e678f80c60e710a096b944c98ef4cf3bad5fe191
eleonore <eleonore@sociablelabs.com>
2015-09-15 18:41:45 -0700

    Update database function

--- controller.go
+++ controller.go
@@ -6,7 +6,6 @@ import (
     "github.com/gorilla/mux"
     "encoding/json"
     "github.com/jinzhu/gorm"
-    "database/sql"
     "io"
     "log"
     "strings"
@@ -17,48 +16,14 @@ var (
     Gdb gorm.DB
 )
 
-func GetTransaction() *gorm.DB {
-    txn := Gdb.Begin()
-        if txn.Error != nil {
-            panic(txn.Error)
-        }
-        return txn
-}
-
-func CommitTransaction(txn *gorm.DB) bool{
-    if txn == nil {
-        return false
-    }
-    txn.Commit()
-    if err := txn.Error; err != nil && err != sql.ErrTxDone {
-        Rollback(txn)
-        return false
-    }
-    return true
-}
-
-func Rollback(txn *gorm.DB) bool{
-   if txn == nil {
-        return false
-    }
-    txn.Rollback()
-    if err := txn.Error; err != nil && err != sql.ErrTxDone {
-        return false
-    }
-    txn = nil
-    return true
-}
-
-
 func Index(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintln(w, "Hello World!")
 }
 
 func TodoShow(w http.ResponseWriter, r *http.Request) {
-    var txn *gorm.DB = GetTransaction();
     var todos []Todo
 
-    err := txn.Find(&todos).Error  
+    err := Gdb.Find(&todos).Error  
     if err != nil {
         panic(err)
     }
@@ -93,18 +58,17 @@ func TodoCreate(w http.ResponseWriter, r *http.Request) {
         w.Header().Set("Content-Type", "application/json; charset=UTF-8")
         w.WriteHeader(http.StatusMethodNotAllowed)
     } else {
-        var txn *gorm.DB = GetTransaction();
-        txn.Create(&todo)
-        succes := CommitTransaction(txn)
-        if (succes) {
+        //var txn *gorm.DB = GetTransaction();
+        err = Gdb.Create(&todo).Error  
+       if err != nil {
+            w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+            w.WriteHeader(http.StatusConflict)    
+        } else {
             w.Header().Set("Content-Type", "application/json; charset=UTF-8")
             w.WriteHeader(http.StatusOK)
             if err := json.NewEncoder(w).Encode(todo); err != nil {
                 panic(err)
             }
-        } else {
-            w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-            w.WriteHeader(http.StatusConflict)
         }
     }
 }
@@ -115,8 +79,7 @@ func TodoUpdate(w http.ResponseWriter, r *http.Request) {
 
     vars := mux.Vars(r)
     todoId := vars["todoId"]
-    var txn *gorm.DB = GetTransaction();
-    err := txn.Where("id = ?", todoId).Find(&todo).Error       
+    err := Gdb.Where("id = ?", todoId).Find(&todo).Error       
     if err != nil {
         w.Header().Set("Content-Type", "application/json; charset=UTF-8")
         w.WriteHeader(http.StatusMethodNotAllowed) 
@@ -135,18 +98,16 @@ func TodoUpdate(w http.ResponseWriter, r *http.Request) {
         }
         todo.Name = todoUpdate.Name
         todo.State = todoUpdate.State
-        txn.Update(&todo)
-        CommitTransaction(txn)
-        succes := CommitTransaction(txn)
-        if (succes) {
+        err = Gdb.Update(&todo).Error
+        if err != nil {
+            w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+            w.WriteHeader(http.StatusConflict)
+        } else {
             w.Header().Set("Content-Type", "application/json; charset=UTF-8")
             w.WriteHeader(http.StatusOK)
             if err := json.NewEncoder(w).Encode(todo); err != nil {
                 panic(err)
             }
-        } else {
-            w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-            w.WriteHeader(http.StatusConflict)
         }
     }
 }
@@ -155,19 +116,18 @@ func TodoDelete(w http.ResponseWriter, r *http.Request) {
     var todo Todo
     vars := mux.Vars(r)
     todoId := vars["todoId"]
-    var txn *gorm.DB = GetTransaction();
-    err := txn.Where("id = ?", todoId).Find(&todo).Error       
+    err := Gdb.Where("id = ?", todoId).Find(&todo).Error       
+    if err != nil {
+        w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+        w.WriteHeader(http.StatusConflict)
+    }
+    err = Gdb.Delete(&todo).Error
     if err != nil {
          w.Header().Set("Content-Type", "application/json; charset=UTF-8")
         w.WriteHeader(http.StatusConflict)
-    }
-    txn.Delete(&todo)
-    succes := CommitTransaction(txn)
-    if (succes) {
+    } else {
+    
         w.Header().Set("Content-Type", "application/json; charset=UTF-8")
         w.WriteHeader(http.StatusOK)
-    } else {
-        w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-        w.WriteHeader(http.StatusConflict)
     }
 }
